5b638afd31abd404dd4dda3f4c215363cfa9a956
habib24457 <habiburehman390@gmail.com>
2022-12-29 04:38:54 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { colors } from "./src/theme/colors";
+import { spacing } from "./src/theme/spacing";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Antonio-Medium": require("./assets/fonts/Antonio-Medium.ttf"),
+    "LeagueSpartan-Bold": require("./assets/fonts/LeagueSpartan-Bold.ttf"),
+    "LeagueSpartan-Regular": require("./assets/fonts/LeagueSpartan-Regular"),
+  });
+
+  if (!loaded) {
+    return <Text>Font is loading...</Text>;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ marginTop: spacing[4] }}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.darkOrange,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
